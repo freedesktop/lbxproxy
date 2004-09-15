6df140632e5c28c496208895337464d58825fddd
Alexander Gottwald <alexander.gottwald@s1999.tu-chemnitz.de>
2004-09-15 16:34:16 +0000

    Pull XORG-6_8_0 to CYGWIN branch

--- di/options.c
+++ di/options.c
@@ -1,4 +1,4 @@
-/* $XdotOrg: xc/programs/lbxproxy/di/options.c,v 1.1.4.3.4.1 2004/03/04 17:48:50 eich Exp $ */
+/* $XdotOrg: xc/programs/lbxproxy/di/options.c,v 1.2 2004/04/23 19:54:33 eich Exp $ */
 /* $Xorg: options.c,v 1.4 2001/02/09 02:05:31 xorgcvs Exp $ */
 /*
 
